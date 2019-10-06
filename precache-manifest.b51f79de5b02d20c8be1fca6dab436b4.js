6ae904d57b8d5736a876867c6c0a39980e6ea16f
Anurag Phadke <anuragphadke19@gmail.com>
2019-10-06 18:59:19 -0400

    Updates

---
+++ precache-manifest.b51f79de5b02d20c8be1fca6dab436b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b36b378176189d8fe059180ec8276c",
+    "url": "/go-roman/index.html"
+  },
+  {
+    "revision": "3a59a48718c9b28d8bf0",
+    "url": "/go-roman/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7bb2a866c297d3378336",
+    "url": "/go-roman/static/js/2.ee14105f.chunk.js"
+  },
+  {
+    "revision": "3a59a48718c9b28d8bf0",
+    "url": "/go-roman/static/js/main.2364fc03.chunk.js"
+  },
+  {
+    "revision": "48388fa7f61568d05421",
+    "url": "/go-roman/static/js/runtime-main.76f6b0df.js"
+  }
+]);

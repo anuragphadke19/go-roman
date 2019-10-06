04d221318b09e70f87556f6c62e5b713a40cb558
Anurag Phadke <anuragphadke19@gmail.com>
2019-10-06 18:48:42 -0400

    added text box and a live date

--- src/go_roman/GoRoman.js
+++ src/go_roman/GoRoman.js
@@ -1,16 +1,56 @@
 import React, {Component} from 'react';
 
 export default class FirstComponent extends Component{
-    constructor(props){
-        super(props)
+
+    handleChange = (e) => {
+        this.setState({
+            textBoxValue: e.target.value
+        });
     }
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            time: new Date().toLocaleTimeString(),
+            textBoxSug: "Please type a number"
+        };
+    }
+
+    tick() {
+        this.setState(prevState => ({
+          time: new Date().toLocaleTimeString()
+        }));
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    
     render(){
-        const element = (<div>No more text</div>)
+        const inputElem = ( <div>
+            <input 
+                type="text"
+                placeholder={this.state.textBoxSug}
+                onChange={this.handleChange}/>
+        </div>);
+        const textElem = (<div>{this.state.textBoxValue}</div>);
+
+        const timeElem = (<div>
+                            <h4>It is : </h4><i>{this.state.time}</i>
+                        </div>);
+
         return (<div className="comptext">
-            <h3>GO ROMAN</h3>
-            {this.props.displaytext}
-            {element}
-        </div>)
+                <h3>GO ROMAN</h3>
+                {this.props.displaytext}
+                {inputElem}
+                {textElem}
+                {timeElem}
+            </div>
+           
+        );
     }
 }
